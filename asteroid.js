6a97f4e5d7612c357340de9f31a06b0227db813e
yhengkang <yhengkang@gmail.com>
2013-11-18 16:19:04 -0800

    asteroids now spawn at edge of map

--- asteroid.js
+++ asteroid.js
@@ -10,15 +10,17 @@
   Asteroid.AVERAGE_RADIUS = 25;
   Asteroid.MIN_RADIUS = 10;
   Asteroid.COLOR = "black";
-  Asteroid.SPEED = 10;
+  Asteroid.AVERAGE_SPEED = 10;
+  Asteroid.MIN_SPEED = 10;
 
   Asteroid.randomAsteroid = function(dimX, dimY){
     var that = this;
     return new Asteroid(
-    [ (Math.random() * dimX), (Math.random() * dimY) ],
+      randomEdgePosition(dimX, dimY),
       randomVel(),
       randomSize(),
-      Asteroid.COLOR );
+      Asteroid.COLOR 
+    );
   };
 
   var randomVel = function() {
@@ -32,6 +34,19 @@
       radius = Math.random()*Asteroid.AVERAGE_RADIUS;
     }
     return radius;
+  }
+
+  var randomEdgePosition = function(dimX, dimY) {
+    var diceRoll = Math.random();
+    if(diceRoll < 0.25){
+      return [1, dimY*Math.random()];
+    } else if (diceRoll < 0.5) {
+      return [dimX-1, dimY*Math.random()];
+    } else if (diceRoll < 0.75) {
+      return [dimX*Math.random(), 1];
+    } else {
+      return [dimX*Math.random(), dimY-1];
+    }
   }
 
 })(this);
